0613a04ad7de2138d97a8046dcbe1ca9c695d98c
Vansh Saxena <vansh.saxena_cs.aiml24@gla.ac.in>
2026-02-11 09:52:37 +0530

    Code push

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include<Wire.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    if (!display.begin(SSD1306_SWITCHCAPVCC,OLED_ADDR)) {
+        Serial.println("OLED NOT FOUND");
+        while (true);
+    }
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0,0);
+    display.println("Arduino UNO R4");
+    display.println("OLED with I2C");
+    display.println("HELLO STUDENTS!");
+    display.display();
 }
 
 void loop() {
-// write your code here
+
 }
